64a803c2c41ee0a92f1688273eb50bb03589f008
Alin Roaua <stefania_stefii97@yahoo.com>
2020-05-12 23:54:59 +0200

    pages added

---
+++ script.js
@@ -0,0 +1,61 @@
+// Navigation
+// Responsive Toggle Navigation =============================================
+let menuIcon = document.querySelector(".menuIcon");
+let nav = document.querySelector(".overlay-menu");
+
+menuIcon.addEventListener("click", () => {
+  if (nav.style.transform != "translateX(-10%)") {
+    nav.style.transform = "translateX(-10%)";
+    nav.style.transition = "transform 0.2s ease-out";
+  } else {
+    nav.style.transform = "translateX(-100%)";
+    nav.style.transition = "transform 0.2s ease-out";
+  }
+});
+
+// Toggle Menu Icon ========================================
+let toggleIcon = document.querySelector(".menuIcon");
+
+toggleIcon.addEventListener("click", () => {
+  if (toggleIcon.className != "menuIcon toggle") {
+    toggleIcon.className += " toggle";
+  } else {
+    toggleIcon.className = "menuIcon";
+  }
+});
+
+// Close the mobile menu when you click a menu item
+let mobileClose = document.querySelector("#menu-mobile");
+mobileClose.addEventListener("click", function () {
+  if (nav.style.transform != "translateX(-10%)") {
+    nav.style.transform = "translateX(-10%)";
+    nav.style.transition = "transform 0.2s ease-out";
+  } else {
+    nav.style.transform = "translateX(-100%)";
+    nav.style.transition = "transform 0.2s ease-out";
+  }
+
+  if (toggleIcon.className != "menuIcon toggle") {
+    toggleIcon.className += " toggle";
+  } else {
+    toggleIcon.className = "menuIcon";
+  }
+});
+
+// When you scroll down 20px from the top of the page,you can see the button(built-in JavaScript's event  )
+window.onscroll = function () {
+  scroll();
+};
+
+function scroll() {
+  if (document.documentElement.scrollTop > 20) {
+    document.getElementById("Btn-top").style.display = "block";
+  } else {
+    document.getElementById("Btn-top").style.display = "none";
+  }
+}
+
+// When you click on the button,you will see the top of the page
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
