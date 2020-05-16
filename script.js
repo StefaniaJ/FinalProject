17757cfd9573fa699b8d901a31772d4b0dacbe80
Ștefania S <stefania_stefii97@yahoo.com>
2020-05-16 19:53:20 +0200

    effect buttons

--- script.js
+++ script.js
@@ -62,11 +62,9 @@ function topFunction() {
 
 // References page
 
-// Fetch data from  Google Sheets as JSON for Crunch Website
+// Fetch data from Google Sheets as JSON for Crunch Website
 
-const LOADING_IMAGE = "images/loading.gif";
-const IMG_WEBSITES = "url('images/cogwheel (1).png')";
-const IMG_GRAPHICS = "url('images/cogwheel (1).png')";
+// const LOADING_IMAGE = "images/loading.gif";
 
 // The main HTML Element and the template
 const main = document.querySelector("#references-wrapper");
@@ -74,18 +72,22 @@ const template = document.querySelector("#template-references").content;
 // The menu
 const menu = document.getElementById("buttons-wrapper");
 const allLink = document.querySelector("#all");
+const websitesLink = document.querySelector("#websites");
+const graphicsLink = document.querySelector("#graphics");
+
 // The googleSheet link
 const link =
   "https://spreadsheets.google.com/feeds/list/1o1okTGFAMCRxodaTFQbERyFNYnITnpmdGpbW62YnBCU/od6/public/values?alt=json";
 
-// function to fetch the movies list
-// from Google Sheets
+// function to fetch the references list
+
 function loadJSON(link) {
   fetch(link)
     .then((e) => e.json())
-    .then((data) => data.feed.entry.forEach(displayReferencesData));
+    .then((data) => data.feed.entry.forEach(showData));
 }
-function displayReferencesData(data) {
+
+function showData(data) {
   const clone = template.cloneNode("true");
   clone.querySelector(".title").textContent = data.gsx$name.$t;
   clone.querySelector(".description").textContent = data.gsx$description.$t;
@@ -96,3 +98,22 @@ function displayReferencesData(data) {
   main.appendChild(clone);
 }
 loadJSON(link);
+
+// Run this function with 1 second delay
+// adds-removes the active class on the menu items
+setTimeout(function runDelay() {
+  const items = menu.querySelectorAll("div");
+
+  // Initialize the active class on the first menu item
+  items[0].classList.add("active");
+
+  // Loop through the buttons and add the active class to the current/clicked button
+  for (let i = 0; i < items.length; i++) {
+    // desktop menu
+    items[i].addEventListener("click", function () {
+      let current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace("active", "");
+      this.className += "active";
+    });
+  }
+}, 1000);
