616b4c1e340477c1a69af7d196ba5f7306b25b81
Ștefania S <stefania_stefii97@yahoo.com>
2020-05-18 02:42:57 +0200

    footer text added

--- script.js
+++ script.js
@@ -3,14 +3,13 @@
 window.addEventListener("DOMContentLoaded", start);
 
 // References page
-
-// const LOADING_IMAGE = "images/loading.gif";
 const main = document.querySelector("#references-wrapper");
 const template = document.querySelector("#template-references").content;
 const menu = document.getElementById("buttons-wrapper");
 const allLink = document.querySelector("#all");
 const websitesLink = document.querySelector("#websites");
 const graphicsLink = document.querySelector("#graphics");
+const LOADING_IMAGE = "images/loading.gif";
 const link =
   "https://spreadsheets.google.com/feeds/list/1o1okTGFAMCRxodaTFQbERyFNYnITnpmdGpbW62YnBCU/od6/public/values?alt=json";
 
@@ -18,8 +17,10 @@ const link =
 
 // function to fetch the references list
 
-function start() {}
-loadJSON(link);
+function start() {
+  loadJSON(link);
+  setLoading();
+}
 
 function loadJSON(link) {
   fetch(link)
@@ -30,17 +31,15 @@ function loadJSON(link) {
 }
 
 function showData(data) {
+  // hide the loading image
+  hideLoading();
+
   const clone = template.cloneNode("true");
   console.log(data);
   clone.querySelector(".name").textContent = data.gsx$name.$t;
   // clone.querySelector(".description").textContent = data.gsx$description.$t;
   const img = data.gsx$image.$t;
-  let imgM = clone
-    .querySelector(".card-img")
-    .setAttribute("src", "../images/" + img);
   clone.querySelector(".card-img").setAttribute("src", "../images/" + img);
-
-  let all_links = document.querySelectorAll(".card a");
 
   if (data.gsx$category.$t == "websites") {
     clone.querySelector(".link-btn").textContent = "See website";
@@ -59,7 +58,6 @@ function showData(data) {
 
   main.appendChild(clone);
 }
-// loadJSON(link);
 
 // Filter Websites
 websitesLink.addEventListener("click", () => {
@@ -98,6 +96,32 @@ for (let i = 0; i < items.length; i++) {
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
+}
+
+// Create the Loading Indicator
+let loadingIndicator = "";
+createLoadingIndicator();
+
+// function to display the loading image
+function setLoading() {
+  loadingIndicator.style.display = "block";
+}
+
+// Function to Hide Loading Indicator / Show title
+function hideLoading() {
+  // Hide the Loading Indicator
+  loadingIndicator.style.display = "none";
+}
+
+// function to create the loadingIndicator
+function createLoadingIndicator() {
+  // Create the Loading Indicator
+  let createImage = document.createElement("img");
+  createImage.setAttribute("id", "loading");
+  createImage.src = LOADING_IMAGE;
+  main.appendChild(createImage);
+  // Get a reference to the loading indidator
+  loadingIndicator = document.getElementById("loading");
 }
 
 // Navigation
