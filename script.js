8a8da5075a099761e3b55f62eae2217fa61bbad9
Ștefania S <stefania_stefii97@yahoo.com>
2020-05-25 20:19:05 +0200

    services pages added

--- script.js
+++ script.js
@@ -44,6 +44,22 @@ mobileClose.addEventListener("click", function () {
   }
 });
 
+// Loop through all dropdown buttons to toggle between hiding and showing its dropdown content
+let dropdown = document.getElementsByClassName("dropdown-btn");
+let i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
 // When you scroll down 20px from the top of the page,you can see the button(built-in JavaScript's event  )
 window.onscroll = function () {
   scroll();
