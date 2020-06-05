d8ab5d479409546cdffb5b6413c919a1d98a44e2
Ștefania S <stefania_stefii97@yahoo.com>
2020-06-05 06:27:01 +0200

    details added

--- script.js
+++ script.js
@@ -72,18 +72,3 @@ function topFunction() {
 }
 
 const form = document.querySelector("#form");
-
-// When the user scrolls the page, execute myFunction
-// Inspiration: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_indicator
-window.onscroll = function () {
-  myFunction();
-};
-
-function myFunction() {
-  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  let height =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight;
-  let scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
